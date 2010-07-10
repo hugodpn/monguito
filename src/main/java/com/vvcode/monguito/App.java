e7dc0d4959f87470a4878433d44ccceff826935f
Hugo Pacini <hugo@brasil.(none)>
2010-07-09 21:03:35 -0300

    Initialize application.

---
+++ src/main/java/com/vvcode/monguito/App.java
@@ -0,0 +1,13 @@
+package com.vvcode.monguito;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
