638e84fbad1092a0362c25e924bfcd56f90be855
Hugo Pacini <hugodpn@yahoo.com.ar>
2010-07-20 20:05:05 -0300

    Fix version.

--- src/main/java/com/vvcode/monguito/App.java
+++ src/main/java/com/vvcode/monguito/App.java
@@ -1,7 +1,6 @@
 package com.vvcode.monguito;
 
 import com.vvcode.monguito.ui.MonguitoUI;
-import javax.swing.JFrame;
 import javax.swing.UIManager;
 
 public class App {
@@ -9,14 +8,14 @@ public class App {
     public static void main(String[] args) {
         try {
 
-          // Set cross-platform Java L&F (also called "Metal")
-        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            // Set cross-platform Java L&F (also called "Metal")
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
 
-        // Set System L&F
+            // Set System L&F
 //        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
-        MonguitoUI monguitoUI = new MonguitoUI();
-        monguitoUI.setVisible(true);
+            MonguitoUI monguitoUI = new MonguitoUI();
+            monguitoUI.setVisible(true);
 
         } catch (Exception e) {
             System.out.println(e.toString());
